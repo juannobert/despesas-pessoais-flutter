e50ed761294336f150e25247370ef395f024fe66
Juan Nobert <nobert407@gmail.com>
2023-03-20 16:56:12 -0300

    Melhorando estilo da lista de transações

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,47 @@ class MyHomePage extends StatelessWidget{
         children: 
           _transactions.map((tr) {
             return Card(
-              child : Text(tr.title)
+              child : Row(children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${tr.value.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple
+                    ),
+                    ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tr.title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),),
+                    Text(
+                      tr.date.toString(),
+                      style: const TextStyle(
+                        color: Colors.grey
+                      )
+                    )
+                  ],
+                )
+              ],
+              )
             );
           }).toList() // Retorna uma lista de cards que são widgets
        )
