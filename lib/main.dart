7aa5f42d9ff78aa1ac7cbe7e68d98f1cbef97745
Juan Nobert <nobert407@gmail.com>
2023-03-20 17:06:09 -0300

    Formatando data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -89,7 +90,7 @@ class MyHomePage extends StatelessWidget{
                         fontWeight: FontWeight.bold
                       ),),
                     Text(
-                      tr.date.toString(),
+                      DateFormat('d MMM y').format(tr.date),
                       style: const TextStyle(
                         color: Colors.grey
                       )
